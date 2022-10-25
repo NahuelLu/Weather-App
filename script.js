5ce527952e00944cb40369ac2a31b6a1cfe13d16
NahuelLu <nahuellucana1999@gmail.com>
2022-10-24 23:32:41 -0300

    Delete input city and Add display weather basic

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const countryInput = document.querySelector('#country');
-const cityInput = document.querySelector('#city');
+const weatherContainer = document.querySelector('.weather-container');
+const clearInputs = ()=>{ countryInput.value = '';cityInput.value=''}
 
-const getWeatherFrom = async(country,city)=>{
-    const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q='+city+','+country+'&appid=c0a13cade9d5eb56c747b5fd429be5de')
-    const data = await response.json();
-    const lat = data[0].lat;
-    const lon = data[0].lon;
-    const responseWeatherStats = await fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=c0a13cade9d5eb56c747b5fd429be5de')
-    const weatherData = await responseWeatherStats.json();
+const getWeatherFrom = async(location)=>{
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+location+'&units=metric&appid=c0a13cade9d5eb56c747b5fd429be5de')
+    const weatherData = await response.json();
     return weatherData;
+}
+const displayWeatherToPage= (weatherData)=>{
+    const countryNameTitle = document.createElement('h1');
+    const dataContainer = document.createElement('div');
+    weatherContainer.appendChild(countryNameTitle);
+    countryNameTitle.textContent= weatherData.name +' Current Weather';
+    weatherContainer.appendChild(dataContainer);
+    dataContainer.className='weather-stats-container';
+    dataContainer.textContent='Temp: '+ weatherData.main.temp;
 }
 const getWeather = (e)=>{
     e.preventDefault();
-    const country = countryInput.value;
-    const city = cityInput.value;
-    getWeatherFrom(country,city).then(data => {
+    const location = countryInput.value;
+    getWeatherFrom(location).then(data => {
         console.log(data);
-        console.log(data.main.feels_like);
+        displayWeatherToPage(data);
     });
+    clearInputs();
 }
 
 const searchWeatherBtnUser = document.querySelector('#search-btn');
