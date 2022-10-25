95a5d814e311ce4e7beeaa05b40d204fbcd138b1
NahuelLu <nahuellucana1999@gmail.com>
2022-10-24 22:35:58 -0300

    Add getWeather function to search weather from user input

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const countryInput = document.querySelector('#country');
+const cityInput = document.querySelector('#city');
+
 const getWeatherFrom = async(country,city)=>{
     const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q='+city+','+country+'&appid=c0a13cade9d5eb56c747b5fd429be5de')
     const data = await response.json();
@@ -7,7 +10,15 @@ const getWeatherFrom = async(country,city)=>{
     const weatherData = await responseWeatherStats.json();
     return weatherData;
 }
-getWeatherFrom('Argentina','Buenos Aires').then(data => {
-    console.log(data);
-    console.log(data.main);
-})
+const getWeather = (e)=>{
+    e.preventDefault();
+    const country = countryInput.value;
+    const city = cityInput.value;
+    getWeatherFrom(country,city).then(data => {
+        console.log(data);
+        console.log(data.main.feels_like);
+    });
+}
+
+const searchWeatherBtnUser = document.querySelector('#search-btn');
+searchWeatherBtnUser.addEventListener('click',getWeather);
